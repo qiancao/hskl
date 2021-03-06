39e13c37908031761e3a8a602fe92f49386251dc
Qian Cao <63546190+qiancao@users.noreply.github.com>
2021-03-06 08:27:39 -0500

    added scikit-image to dependency

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setuptools.setup(
                       'spectral',
                       'h5py',
                       'scipy',
-                      'tqdm']
+                      'tqdm',
+                      'scikit-image']
 )
