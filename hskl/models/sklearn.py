3aac0394aac6ec15bb4abed39bc58880063157ad
Qian Cao <qcao.dev@gmail.com>
2021-03-26 14:07:02 -0400

    models/sklearn

--- hskl/models/sklearn.py
+++ hskl/models/sklearn.py
@@ -19,6 +19,30 @@ _regressor = dict(all_estimators(type_filter='regressor'))
 _cluster = dict(all_estimators(type_filter='cluster'))
 _transformer = dict(all_estimators(type_filter='transformer'))
 
-def get_args(f):
-    # Get argument signature (e.g. get_args(_classifier['RandomForestClassifier']))
-    return inspect.signature(f)
+def get_params(f):
+    """
+    Returns parameters and default values to class constructor
+    
+    Example:
+        get_params(_classifier['RandomForestClassifier'])
+        >>
+            {'n_estimators': 100,
+             'criterion': 'gini',
+             ...
+             'ccp_alpha': 0.0,
+             'max_samples': None}
+
+    Parameters
+    ----------
+    f : class
+        Scikit-learn estimator class
+
+    Returns
+    -------
+    param_dict : dictionary
+        dictionary of parameters and default values
+
+    """
+    sig = inspect.signature(f)
+    param_dict = {key:value.default for (key,value) in dict(sig.parameters).items()}
+    return param_dict
